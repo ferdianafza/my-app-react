44a049e31970ce575b753b6321252184916ec122
Ferdian <farisafza201002@gmail.com>
2020-03-10 19:40:38 +0700

    add router

---
+++ src/Profile/MyProfile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from "axios";
+
+export default class MyProfile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: null,
+      fullname: "",
+      email: ""
+    };
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('token');
+
+    axios
+    .get('http://localhost:3000/api/v1/users/myprofile', { headers: {"Authorization" : ${token}} })
+    .then(response => {
+      console.log(response.data);
+      this.setState('id', response.data.id)
+      this.setState('fullname', response.data.fullname)
+      this.setState('email', response.data.email)
+    })
+  }
+
+  render(){
+    const { id, fullname, email } = this.state;
+
+    return (
+      <div>
+        <p>ID : {id}</p>
+        <p>Fullname : {fullname}</p>
+        <p>Email : {email}</p>
+      </div>
+    )
+  }
+}
