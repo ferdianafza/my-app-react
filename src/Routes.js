e06c25f02d57cc8ccac02c7b5ce5d6aac016f24b
Ferdian <farisafza201002@gmail.com>
2020-03-10 19:42:48 +0700

    add file routes

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        
+      </Route>
+    </Switch>
+  );
+}
